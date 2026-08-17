0b4dd16591fc3400d0d5f6896db6880f34bedc58
Karthik Madipalli <madipallikarthik@gmail.com>
2026-08-17 22:14:12 +0530

    feat: build authentication forms

--- lms-frontend/src/components/common/Input.jsx
+++ lms-frontend/src/components/common/Input.jsx
@@ -1,8 +1,10 @@
-function Input({ placeholder, type = 'text' }) {
+function Input({ placeholder, type = 'text', value, onChange }) {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      value={value}
+      onChange={onChange}
       className="w-full rounded-lg border border-gray-300 p-3 outline-none focus:border-blue-500"
     />
   )
--- lms-frontend/src/routes/AppRoutes.jsx
+++ lms-frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/Auth/Login'
+import Register from '../pages/Auth/Register'
 import UITest from '../pages/UITest'
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/ui-test" element={<UITest />} />
     </Routes>
   )
